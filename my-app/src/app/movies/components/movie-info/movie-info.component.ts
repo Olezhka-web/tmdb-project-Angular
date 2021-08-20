43cb82fcf52eacebcc5491ad65a10464b4bf3f27
Olezhka-web <zcegthrkfcghj5@gmail.com>
2021-08-20 22:36:26 +0300

    Update

--- my-app/src/app/movies/components/movie-info/movie-info.component.ts
+++ my-app/src/app/movies/components/movie-info/movie-info.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from "@angular/router";
 import {ResultsModel} from "../../../models/results.model";
 import {GenreModel} from "../../../models/genre.model";
 import {MovieItemModel} from "../../../models/movie-item.model";
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-movie-info',
@@ -19,9 +20,12 @@ export class MovieInfoComponent implements OnInit {
 
   genre_ids: number[] = [];
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  checked: boolean;
+
+  constructor(private activatedRoute: ActivatedRoute, private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.dataCell.subscribe(value => this.checked = value);
     this.activatedRoute.data.subscribe(value => this.genres = value.genresResolveService);
     this.activatedRoute.data.subscribe(value => {
       if(value.moviesItemResolveService.state){
